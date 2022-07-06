9782029aa8d4fa24db9bff05f288cef146a56818
YoungPhilGim <youngphil.gim@kasset.co.kr>
2022-07-06 16:03:06 +0900

    fixed

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import TodoCard from './components/TodoCard.vue';
   <Navbar brand="The Todo" />
   <div class="container">
     <div class="row">
-      <div class="col-12 col-lg-4">
+      <div class="col-12 col-lg-4 mt-4">
         <TodoCard />
       </div>
     </div>
