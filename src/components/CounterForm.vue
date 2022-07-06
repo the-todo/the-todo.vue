dbd11c4a709896333fbc70f3d309df715367a97b
YoungPhilGim <youngphil.gim@kasset.co.kr>
2022-07-06 15:28:22 +0900

    Initial commit

---
+++ src/components/CounterForm.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { reactive } from 'vue';
+
+const todos = reactive({
+  list: [],
+  // 'ALL' | 'FINISHED' | 'UNFINISHED'
+  filter: 'ALL',
+  nextId: 0,
+});
+
+const addTodo = (text) => {
+  const { list, id } = todos;
+  list.push({ text, id: id + 1, isFinished: false });
+};
+
+const setFilter = (filter) => {
+  todos.filter = filter;
+};
+</script>
+
+<template>
+  <div class="d-flex justify-content-between align-items-center mt-3">
+    <button
+      class="btn btn-outline-primary"
+      @click="decrement"
+    >-</button>
+    {{ count }}
+    <button
+      class="btn btn-outline-primary"
+      @click="increment"
+    >+</button>
+  </div>
+</template>
---
+++ src/components/InputNumber.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  id: String,
+  label: String,
+  placeholder: String,
+  value: Number,
+});
+</script>
+
+<template>
+  <div>
+    <label
+      class="form-label"
+      :for="id"
+    >{{ label }}</label>
+    <input
+      type="number"
+      class="form-control"
+      :id="id"
+      :placeholder="placeholder"
+      :value="value"
+      @input="$emit('input-value', parseInt($event.target.value))"
+    >
+  </div>
+</template>
